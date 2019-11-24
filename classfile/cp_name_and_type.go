aa2e8002eac3ede46093d5422294c7c61c1bbbad
skykiss <764213885@qq.com>
2019-11-24 23:04:29 +0800

    增加注释

--- classfile/cp_name_and_type.go
+++ classfile/cp_name_and_type.go
@@ -1,15 +1,12 @@
 package classfile
 
+// 指向字段或者方法的名称和描述符
 type ConstantNameAndTypeInfo struct {
-	nameIndex uint16  // 指向该字段或者方法的名称常量项的索引
-	descIndex uint16  // 指向该字段或者方法的描述符常量项的索引
+	nameIndex uint16 // 指向该字段或者方法的名称常量项的索引
+	descIndex uint16 // 指向该字段或者方法的描述符常量项的索引
 }
 
-func ( c *ConstantNameAndTypeInfo) readInfo(reader *ClassReader) {
+func (c *ConstantNameAndTypeInfo) readInfo(reader *ClassReader) {
 	c.nameIndex = reader.readUint16()
 	c.descIndex = reader.readUint16()
 }
-
-
-
-
