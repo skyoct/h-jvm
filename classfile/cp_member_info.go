9016d8942b5d488cdd743de66ffbc9910bf46dd3
skykiss <764213885@qq.com>
2019-11-29 19:45:16 +0800

    抽取共同代码，精简结构

--- classfile/cp_member_info.go
+++ classfile/cp_member_info.go
@@ -1,60 +1,36 @@
 package classfile
 
+// 抽取Fieldref Methodref interfaceMethodref的共同结构
+type ConstantMemberInfo struct {
+	cp               ConstantPool
+	classIndex       uint16
+	nameAndTypeIndex uint16
+}
+
+func (c *ConstantMemberInfo) ClassName() string {
+	return c.cp.getClassName(c.classIndex)
+}
+
+func (c *ConstantMemberInfo) NameAndDescriptor() (string, string) {
+	return c.cp.getNameAndType(c.nameAndTypeIndex)
+}
+
+func (c *ConstantMemberInfo) readInfo(reader *ClassReader) {
+	c.classIndex = reader.readUint16()
+	c.nameAndTypeIndex = reader.readUint16()
+}
 
 //CONSTANT_Fieldref_info表示字段符号引用
 type ConstantFieldrefInfo struct {
-	cp ConstantPool
-	classIndex uint16
-	nameAndTypeIndex uint16
+	ConstantMemberInfo
 }
-func (c *ConstantFieldrefInfo) readInfo(reader *ClassReader) {
-	c.classIndex = reader.readUint16()
-	c.nameAndTypeIndex = reader.readUint16()
-}
-
-func (c *ConstantFieldrefInfo) ClassName() string {
-	return c.cp.getClassName(c.classIndex)
-}
-
-func (c *ConstantFieldrefInfo) NameAndDescriptor() (string, string) {
-	return c.cp.getNameAndType(c.nameAndTypeIndex)
-}
-
 
 // CONSTANT_Methodref_info表示普通（非接口）方法符号引用，
 type ConstantMethodrefInfo struct {
-	cp ConstantPool
-	classIndex uint16
-	nameAndTypeIndex uint16
-}
-func (c *ConstantMethodrefInfo) readInfo(reader *ClassReader) {
-	c.classIndex = reader.readUint16()
-	c.nameAndTypeIndex = reader.readUint16()
-}
-
-func (c *ConstantMethodrefInfo) ClassName() string {
-	return c.cp.getClassName(c.classIndex)
-}
-
-func (c *ConstantMethodrefInfo) NameAndDescriptor() (string, string) {
-	return c.cp.getNameAndType(c.nameAndTypeIndex)
+	ConstantMemberInfo
 }
 
 // CONSTANT_InterfaceMethodref_info表示接口方法符号引用
 type ConstantInterfaceMethodrefInfo struct {
-	cp ConstantPool
-	classIndex uint16
-	nameAndTypeIndex uint16
-}
-func (c *ConstantInterfaceMethodrefInfo) readInfo(reader *ClassReader) {
-	c.classIndex = reader.readUint16()
-	c.nameAndTypeIndex = reader.readUint16()
-}
-
-func (c *ConstantInterfaceMethodrefInfo) ClassName() string {
-	return c.cp.getClassName(c.classIndex)
-}
-
-func (c *ConstantInterfaceMethodrefInfo) NameAndDescriptor() (string, string) {
-	return c.cp.getNameAndType(c.nameAndTypeIndex)
+	ConstantMemberInfo
 }
