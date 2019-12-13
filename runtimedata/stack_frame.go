1b23b0abf63bfa12fce23b10f229746dbe70e532
skykiss <764213885@qq.com>
2019-12-13 09:45:26 +0800

    添加java虚拟机栈栈帧对方法的引用

--- runtimedata/stack_frame.go
+++ runtimedata/stack_frame.go
@@ -1,5 +1,7 @@
 package runtimedata
 
+import "h-jvm/runtimedata/metaspace"
+
 // 栈帧 每个栈帧包括局部变量表和操作数栈
 type Frame struct {
 	next         *Frame
@@ -7,6 +9,15 @@ type Frame struct {
 	operandStack *OperandStack
 	thread       *Thread
 	nextPc       int
+	method       *metaspace.Method // 指向方法的指针 每个帧都是一个方法
+}
+
+func NewFrame(maxLocals, maxStack uint, method *metaspace.Method) *Frame {
+	return &Frame{
+		localVars:    newLocalVars(maxLocals),
+		operandStack: newOperandStack(maxStack),
+		method:       method,
+	}
 }
 
 func (f *Frame) OperandStack() *OperandStack {
@@ -15,13 +26,6 @@ func (f *Frame) OperandStack() *OperandStack {
 
 func (f *Frame) LocalVars() LocalVars {
 	return f.localVars
-}
-
-func NewFrame(maxLocals, maxStack uint) *Frame {
-	return &Frame{
-		localVars:    newLocalVars(maxLocals),
-		operandStack: newOperandStack(maxStack),
-	}
 }
 
 func (f *Frame) NextPc() int {
@@ -34,4 +38,8 @@ func (f *Frame) SetNextPc(nextPc int) {
 
 func (f *Frame) Thread() *Thread {
 	return f.thread
+}
+
+func (f *Frame) Method() *metaspace.Method {
+	return f.method
 }
