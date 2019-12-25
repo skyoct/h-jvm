2f9d72e6fbd5f0361f9bea3ab1ad6702ccafab92
skykiss <764213885@qq.com>
2019-12-25 21:07:36 +0800

    添加增加到常量池的类型

--- runtimedata/metaspace/constant_pool.go
+++ runtimedata/metaspace/constant_pool.go
@@ -22,7 +22,35 @@ func newConstantPool(class *Class, cfCp classfile.ConstantPool) *ConstantPool {
 		case *classfile.ConstantIntegerInfo:
 			intInfo := cpInfo.(*classfile.ConstantIntegerInfo)
 			constants[i] = intInfo.Value()
+		case *classfile.ConstantFloatInfo:
+			floatInfo := cpInfo.(*classfile.ConstantFloatInfo)
+			constants[i] = floatInfo.Value()
+		case *classfile.ConstantLongInfo:
+			longInfo := cpInfo.(*classfile.ConstantLongInfo)
+			constants[i] = longInfo.Value()
+			i++
+		case *classfile.ConstantDoubleInfo:
+			doubleInfo := cpInfo.(*classfile.ConstantDoubleInfo)
+			constants[i] = doubleInfo.Value()
+			i++
+		case *classfile.ConstantStringInfo:
+			stringInfo := cpInfo.(*classfile.ConstantStringInfo)
+			constants[i] = stringInfo.String()
+		case *classfile.ConstantClassInfo:
+			classInfo := cpInfo.(*classfile.ConstantClassInfo)
+			constants[i] = newClassRef(cp, classInfo)
+		case *classfile.ConstantFieldrefInfo:
+			fieldrefInfo := cpInfo.(*classfile.ConstantFieldrefInfo)
+			constants[i] = newFieldRef(cp, fieldrefInfo)
+		case *classfile.ConstantMethodrefInfo:
+			methodrefInfo := cpInfo.(*classfile.ConstantMethodrefInfo)
+			constants[i] = newMethodRef(cp, methodrefInfo)
+		case *classfile.ConstantInterfaceMethodrefInfo:
+			methodrefInfo := cpInfo.(*classfile.ConstantInterfaceMethodrefInfo)
+			constants[i] = newInterfaceMethodRef(cp, methodrefInfo)
+		default:
 		}
+
 	}
 	return cp
 }
